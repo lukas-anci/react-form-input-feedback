8b9cc417a4d9c47ab8ef8c8a64f9ce0c24a6549e
Lukas <lukas.ancikevicius@gmail.com>
2021-08-17 09:37:56 +0300

    form 2nd input done

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,32 +1,61 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
-  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  const [formIsValid, setFormValid] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
 
-  const enteredNameIsValid = enteredName.trim() !== '';
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const [enterEmail, setEnterEmail] = useState('');
+  const [enterEmailTouched, setEnterEmailTouched] = useState(false);
+
+  const emailValidationRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const nameValidation = /^([a-zA-Z]+\s)*[a-zA-Z]+$/;
+
+  const enteredEmailIsValid = emailValidationRegex.test(enterEmail);
+
+  // const enteredEmailIsValid = !re;
+  const emailInputIsInvalid = !enteredEmailIsValid && enterEmailTouched;
+
+  const enteredNameIsValid =
+    enteredName.trim().length >= 3 && nameValidation.test(nameValidation);
+  const nameInputHasError = !enteredNameIsValid && enteredNameTouched;
+
+  // prideti email input ir prie pavaldinuoti prie esamos logikos
+  // email turi tureti @ ir taska po @.
+
+  useEffect(() => {
+    enteredNameIsValid && enteredEmailIsValid
+      ? setFormIsValid(true)
+      : setFormIsValid(false);
+  }, [enteredNameIsValid, enteredEmailIsValid]);
 
   const nameInputChangeHandler = (event) => {
     console.log('keystroke');
     setEnteredName(event.target.value);
+  };
+
+  const emailInputChangeHandler = (event) => {
+    console.log('email keystroke');
+    setEnterEmail(event.target.value);
   };
   const formSubmissionHandler = (event) => {
     // sustabdyti forma nuo siuntimo nustatytuoju budu
     event.preventDefault();
 
     // formos siuntimas reiskia kad visi laukai yra paliesti
+    setEnterEmailTouched(true);
     setEnteredNameTouched(true);
 
     // validacija
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
 
     // isvalyti input po submit
     setEnteredName('');
     setEnteredNameTouched(false);
+    setEnterEmail('');
+    setEnterEmailTouched(false);
   };
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
@@ -35,13 +64,20 @@ const SimpleInput = (props) => {
       return;
     }
   };
-
-  const nameInputClasses = nameInputIsInvalid
+  const emailInputHandler = (event) => {
+    setEnterEmailTouched(true);
+    if (!enteredEmailIsValid) {
+      return;
+    }
+  };
+  const nameInputClasses = nameInputHasError
     ? 'form-control invalid'
     : 'form-control';
-
+  const emailInputClasses = emailInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
   return (
-    <form onSubmit={formSubmissionHandler}>
+    <form autoComplete="off" onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
@@ -50,13 +86,26 @@ const SimpleInput = (props) => {
           onBlur={nameInputBlurHandler}
           type="text"
           id="name"
+        />{' '}
+        {nameInputHasError && (
+          <p className="error-text">Name must be at least 3 characters long</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="name">Email</label>
+        <input
+          value={enterEmail}
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputHandler}
+          type="email"
+          id="email"
         />
-        {nameInputIsInvalid && (
-          <p className="error-text">Name must not be empty</p>
+        {emailInputIsInvalid && (
+          <p className="error-text">Email must not be empty</p>
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
